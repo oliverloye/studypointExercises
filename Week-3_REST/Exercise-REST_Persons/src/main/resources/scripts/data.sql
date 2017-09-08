7759e998ded6a0581cf617c74f5f9e69b486b99b
Oliver <oliverloye@me.com>
2017-09-08 10:33:41 +0200

    deployment til DO

---
+++ Week-3_REST/Exercise-REST_Persons/src/main/resources/scripts/data.sql
@@ -0,0 +1,3 @@
+insert into PERSON values (null,'Kurt','Wonnegut','123456');
+insert into PERSON values (null,'Jane','Wonnegut','795843');
+insert into PERSON values (null,'Peter','Hansen','78964456');
