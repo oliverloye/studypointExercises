88df14a1ad2fd34048f06f687d2905453ab1cbd1
Oliver <oliverloye@me.com>
2018-02-13 23:12:18 +0100

    opdateret opgave 1+2 og tilføjer opgave 4

--- Week-2_Javascript/JsDay2/jsDay2.js
+++ Week-2_Javascript/JsDay2/jsDay2.js
@@ -1,6 +1,26 @@
+//docBody = document.getElementsByTagName("body")[0];
+//myBodyDivs = docBody.getElementsByTagName("div");
+//myBodyDivs.style.backgroundColor = "blue";
+
+window.onload = standardColor();
+
+function standardColor() {
+    //alert('test1')
+//    var i;
+//    var divElements = document.getElementsByTagName("div");
+//    for (i = 0; i < divElements.length; i++) {
+//        
+//        divElements[i].style.backgroundColor = "blue";
+//    }
+    document.getElementsByTagName("div")[0].style.backgroundColor = "blue";
+    document.getElementsByTagName("div")[1].style.backgroundColor = "blue";
+    document.getElementsByTagName("div")[2].style.backgroundColor = "blue";
+          
+    //alert('test2');
+}
+
 function divColor() {
     document.getElementById("div1").style.backgroundColor = "red";
     document.getElementById("div2").style.backgroundColor = "yellow";
     document.getElementById("div3").style.backgroundColor = "green";
 }
-
